83a69920f761cbd483032ada2965e3f49ac90e70
MatiasMartina <matiasmartina97@gmail.com>
2024-10-08 08:49:55 +0000

    fix: Agregué asignar contraseña con hash

--- RTA_SCRIPT_Examen_20241007/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_C.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo
-echo "Para ejecutar:   ./1_for_lista.sh  2_for_cmd_lista.txt"
-echo
+
+
+
 
 LISTA=$1
-
+USER_HASH=$2
+HASH=$(sudo grep -i $USER_HASH /etc/shadow | awk -F ':' '{print $2}' )
+#echo "$HASH"
+#exit 1
 ANT_IFS=$IFS
 
 IFS=$'\n' #Caracter que usa el for para dar cada vuelta
@@ -16,19 +19,28 @@ IFS=$'\n' #Caracter que usa el for para dar cada vuelta
 for LINEA in `cat $LISTA |  grep -v ^#` 
 
 do
+	echo "Linea: $LINEA"
+	echo
 	USUARIO=$(echo  $LINEA |awk -F ',' '{print $1}')
 	GRUPO=$(echo  $LINEA |awk -F ',' '{print $2}')
 	
 	if [ $(grep "$GRUPO:" /etc/group -c) -eq 0 ]; then
-   		sudo groupadd $GRUPO
- 		echo"grupo $GRUPO creado"
+   		
+		echo "sudo groupadd $GRUPO"
+ 		
+		echo "grupo $GRUPO creado"
 	else
 	    echo "el grupo $GRUPO ya existe"
 	fi
 	
-	
-	sudo useradd -m -s /bin/bash -G $GRUPO $USUARIO
+	if [ $(grep "$USUARIO" /etc/passwd -c) -eq 0 ]; then
 
+	
+		echo "sudo useradd -m -p "$HASH" -s /bin/bash -G $GRUPO $USUARIO"
+		echo "----------------------------------------------"
+	else	
+		echo "el usuario $USUARIO ya existe"
+	fi
 done
 IFS=$ANT_IFS
 
