dbc8a86411c5021ae77190b9fa2e4310d8d1793e
MatiasMartina <matiasmartina97@gmail.com>
2024-10-08 07:54:23 +0000

    ADD: Punto_C, empezado, falta la parte del HASH) y ajustar los permisos

--- RTA_SCRIPT_Examen_20241007/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_C.sh
@@ -1,1 +1,44 @@
 #!/bin/bash
+
+echo
+echo "Para ejecutar:   ./1_for_lista.sh  2_for_cmd_lista.txt"
+echo
+
+LISTA=$1
+
+ANT_IFS=$IFS
+
+IFS=$'\n' #Caracter que usa el for para dar cada vuelta
+
+
+
+
+for LINEA in `cat $LISTA |  grep -v ^#` 
+
+do
+	USUARIO=$(echo  $LINEA |awk -F ',' '{print $1}')
+	GRUPO=$(echo  $LINEA |awk -F ',' '{print $2}')
+	
+	if [ $(grep "$GRUPO:" /etc/group -c) -eq 0 ]; then
+   		sudo groupadd $GRUPO
+ 		echo"grupo $GRUPO creado"
+	else
+	    echo "el grupo $GRUPO ya existe"
+	fi
+	
+	
+	sudo useradd -m -s /bin/bash -G $GRUPO $USUARIO
+
+done
+IFS=$ANT_IFS
+
+
+
+
+
+
+
+
+
+
+
