2f155fd77335109340a7d80ae99a6a02d07ff85d
MatiasMartina <matiasmartina97@gmail.com>
2024-10-09 04:32:34 +0000

    ADD: Punto_C.ssh resultado final de la combinación de los 3 archivos bis

--- RTA_SCRIPT_Examen_20241007/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_C.sh
@@ -6,7 +6,8 @@
 
 LISTA=$1
 USER_HASH=$2
-LISTA_PERMISOS=$3
+
+
 HASH=$(sudo grep -i $USER_HASH /etc/shadow | awk -F ':' '{print $2}' )
 #echo "$HASH"
 #exit 1
@@ -45,6 +46,63 @@ do
 done
 IFS=$ANT_IFS
 
+
+PERMISO_LISTA=$3
+#CARPETA,DUEÑO,GRUPO,PERMISOS
+LISTA_USUARIOS_CARPETAS=$4
+
+
+IFS=$'\n'
+
+#alumno1,/Examenes-UTN/alumno_1
+#alumno2,/Examenes-UTN/alumno_2
+#alumno3,/Examenes-UTN/alumno_3
+#profesores,/Examenes-UTN/profesores
+
+for LINEA in `cat "$LISTA_USUARIOS_CARPETAS" | grep -v ^#`
+do
+        echo
+	echo "Linea: $LINEA"
+        USUARIO=$(echo $LINEA | awk -F ',' '{print $1}')
+        DIRECTORIO=$(echo $LINEA | awk -F ',' '{print $2}')
+
+        echo
+        echo "--------------------"
+        echo
+
+        #DIRECTORIO,USUARIO
+        echo "directorios creados"
+        #sudo su -c "whoami" > ./probando.txt p1c2_2024_A1
+        sudo su -c "whoami" > "$DIRECTORIO"/validar.txt $USUARIO
+	echo "--------------------"
+done
+
+
+
+
+for LINEA_PERMISO in `cat $PERMISO_LISTA | grep -v ^#`
+do
+    echo "Linea permiso: $LINEA_PERMISO"
+    DIRECTORIO=$(echo $LINEA_PERMISO | awk -F ',' '{print $1}')
+    PROPIETARIO=$(echo $LINEA_PERMISO | awk -F ',' '{print $2}')
+    GRUPO=$(echo $LINEA_PERMISO | awk -F ',' '{print $3}')
+    PERMISOS=$(echo $LINEA_PERMISO | awk -F ',' '{print $4}')
+    echo
+    echo "----------------------------------------------------"
+    echo
+
+        echo "Asignando permisos a $DIRECTORIO"
+        sudo chown -R $PROPIETARIO:$GRUPO $DIRECTORIO
+        sudo chmod -R $PERMISOS $DIRECTORIO
+
+        echo "Permisos asignados a $DIRECTORIO"
+        echo
+  done
+
+
+
+
+IFS=$ANT_IFS
 
 
 
